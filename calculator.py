909db8cb4b37436fa0be3ec7445b62f68b9acc7e
Aayush <mishraayu185@gmail.com>
2025-12-17 04:22:45 -0800

    productivity suite calculator has been made

---
+++ calculator.py
@@ -0,0 +1,53 @@
+import math
+
+print("CALCULATOR MANAGER")
+ 
+
+options = {
+    "Arithmatic opertaions":"Choose 1-5",
+    "pi value":"choose 6",
+    "sqrt":"choose 7"
+} 
+print("operations menu:",options)
+choice = int(input("Choose operation :"))
+
+
+def pie(): 
+        print("𝛑",math.pi) 
+
+def sqrt():
+        x = int(input("Enter no for square-root:"))
+        print(math.sqrt(x))           
+
+
+if choice == 6:
+    pie()
+elif choice == 7:
+    sqrt()
+   
+
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+match choice:
+    case 1:
+        print("Addition:",a+b)
+
+    case 2:
+        print("Subtraction:",a-b)
+
+    case 3:
+        print("Multiplication:",a*b)
+
+    case 4:
+        print("Division:",a/b)
+
+    case 5:
+        print("Modulus:",a%b) 
+
+
+    
+
+            
+
+ 
---
+++ main.py
@@ -0,0 +1,8 @@
+print("PERSONAL PRODUCTIVITY SUITE")
+
+# Open with explicit UTF-8 encoding to avoid UnicodeDecodeError on Windows
+
+print("MAIN MENU:\n1. Calculator Tool\n2. Notes Manager\n3. Timer & Stopwatch\n4. File Organizer\n5. Unit Converter\n6. Backup & Restore\n7. Exit")
+
+# with open("calculator.py", 'r', encoding='utf-8') as f:
+#     exec(f.read())
